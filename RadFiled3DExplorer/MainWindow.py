d4fb7ce966b1454980a3a21bf53b9e4620c16d6b
Felix Lehner <lehner04@pad.ptb.de>
2025-03-03 14:33:01 +0100

    adding gauge progress indicator; adding statistics plotting

--- RadFiled3DExplorer/MainWindow.py
+++ RadFiled3DExplorer/MainWindow.py
@@ -17,11 +17,13 @@ import dash_bootstrap_components as dbc
 import webview
 import tkinter as tk
 from tkinter import filedialog
-from RadFiled3D.RadFiled3D import FieldStore
+from RadFiled3D.RadFiled3D import FieldStore, RadiationFieldMetadataHeaderV1
+import numpy as np
 
 
 class MainWindow(FileSystemEventHandler):
     instance = None
+    file_parsing_progress = 0
 
     @staticmethod
     def on_any_event(event):
@@ -69,54 +71,19 @@ class MainWindow(FileSystemEventHandler):
         self.last_directories = set()
         self.dataset_path = os.path.abspath(os.getcwd())  # Default path
 
+        MainWindow.file_parsing_progress = 100
+        MainWindow.last_statistics_calculated = ""
+
         self.app.layout = html.Div([
-            html.Div([
-                html.H3("File Explorer"),
-                html.Div([
-                    dcc.Dropdown(id='directory-list', options=list(self.last_directories), multi=False, placeholder=f"{self.dataset_path}", style={'height': '90%', 'width': '90%'}),
-                    html.Button('...', id='select-directory-btn', style={'width': '10%', 'top': '0%', 'right': '0%', 'position': 'relative'})
-                ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '10%', 'width': '100%'}),
-                html.Ul(id='file-list', style={'height': '75%', 'overflowY': 'scroll', 'listStyleType': 'none', 'padding': '0', 'margin': '0'}),
-                html.Div([
-                    html.Button('<--', id='prev-btn', style={'width': '45%', 'margin-right': '5%'}),
-                    html.Button('-->', id='next-btn', style={'width': '45%'})
-                ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '10%'})
+            html.Div([ # Sidebar with file explorer
+                dcc.Tabs(id='explorer-tabs', value='files', children=[
+                    dcc.Tab(label='Files', value='files', style={'height': '7vh'}),
+                    dcc.Tab(label='Statistics', value='statistics', style={'height': '7vh'})
+                ], style={'height': '7vh'}),
+                html.Div(id='explorer-content')
             ], style={'width': '30%', 'height': '100%', 'float': 'left', 'padding': '10px'}),
-            html.Div([
-                html.Div([
-                    dcc.RadioItems(
-                        id='layer-selection',
-                        options=[
-                            {'label': 'Air Kerma', 'value': 'Kerma'},
-                            {'label': 'Hits', 'value': 'Hits'},
-                            {'label': 'Directions', 'value': 'Directions'},
-                            {'label': 'Errors', 'value': 'Errors'}
-                        ],
-                        value='Kerma',
-                        labelStyle={'display': 'inline-block', 'margin-right': '10px'}
-                    )
-                ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'width': '100%', 'height': '10%', 'padding': '10px'}),
-                html.Div([
-                    html.Div([
-                        dcc.Graph(id='viewer-graph', style={'width': 'calc(100% - 50px)', 'height': '100%', 'float': 'left'}),
-                        html.Div([
-                            dcc.Slider(id='vertical-slider', vertical=True, min=0, max=100, step=1, value=50, marks={0: '0', 100: '100'})
-                        ], style={'width': '50px', 'height': '100%', 'float': 'right', 'display': 'flex'})
-                    ], style={'width': '100%', 'height': '100%', 'display': 'flex'})
-                ], style={'width': '100%', 'height': '80%', 'padding': '10px'}),
-                html.Div([
-                    dcc.RadioItems(
-                        id='component-selection',
-                        options=[
-                            {'label': 'Beam', 'value': 'Beam'},
-                            {'label': 'Scatter', 'value': 'Scatter'},
-                            {'label': 'All', 'value': 'All'}
-                        ],
-                        value='All',
-                        labelStyle={'display': 'inline-block', 'margin-right': '10px'}
-                    ),
-                    dcc.Checklist(id='enable-vertical-slicing', options=[{'label': 'Enable vertical slicing', 'value': 'vertical-slicing'}], value=['vertical-slicing']),
-                ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'width': '100%', 'height': '10%', 'padding': '10px'})
+            html.Div([  # Content of the data display area
+                html.Div(id='data-display-area')
             ], style={'width': '70%', 'height': '100%', 'float': 'right', 'padding': '10px'}),
             dcc.Store(id='window-initialized', data=False),
             # JavaScript snippet to trigger a Dash callback when the window is fully loaded
@@ -127,6 +94,145 @@ class MainWindow(FileSystemEventHandler):
                 });
             ''')
         ], style={'height': '100vh', 'display': 'flex'})
+
+        @self.app.callback(
+            Output('explorer-content', 'children'),
+            [Input('explorer-tabs', 'value')]
+        )
+        def update_explorer_content(tab):
+            if tab == 'files':
+                return html.Div([
+                    html.H3("File Explorer"),
+                    html.Div([
+                        dcc.Dropdown(id='directory-list', options=list(self.last_directories), multi=False, placeholder=f"{self.dataset_path}", style={'height': '90%', 'width': '90%'}),
+                        html.Button('...', id='select-directory-btn', style={'width': '10%', 'top': '0%', 'right': '0%', 'position': 'relative'})
+                    ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '10vh', 'width': '100%'}),
+                    html.Ul(id='file-list', style={'height': '75vh', 'max-height': "75vh", 'overflowY': 'scroll', 'listStyleType': 'none', 'padding': '0', 'margin': '0'}),
+                    html.Div([
+                        html.Button('<--', id='prev-btn', style={'width': '45%', 'margin-right': '5%'}),
+                        html.Button('-->', id='next-btn', style={'width': '45%'})
+                    ], style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '10%', 'margin-bottom': '5%', 'bottom': '5%', 'position': 'relative', 'width': '95%'}),
+                ], style={'height': '90vh', 'width': '100%'})
+            elif tab == 'statistics':
+                return html.Div([
+                    html.H3("Statistics"),
+                    dcc.RadioItems(id='statistics-list',
+                        options=[{'label': 'Energy', 'value': 'energy'}, {'label': 'Angles', 'value': 'angles'}, {'label': 'Source Distance', 'value': 'source_distance'}],
+                        value='energy',
+                        style={'height': '10vh', 'listStyleType': 'none', 'padding': '0', 'margin': '0'}),
+                ])
+
+        @self.app.callback(
+            Output('data-display-area', 'children'),
+            [Input('explorer-tabs', 'value')]
+        )
+        def update_data_display_area(tab):
+            if tab == 'files':
+                return html.Div([
+                    dcc.RadioItems(
+                        id='layer-selection',
+                        options=[
+                            {'label': 'Air Kerma', 'value': 'Kerma'},
+                            {'label': 'Hits', 'value': 'Hits'},
+                            {'label': 'Directions', 'value': 'Directions'},
+                            {'label': 'Errors', 'value': 'Errors'}
+                        ],
+                        value='Kerma',
+                        labelStyle={'display': 'inline-block', 'margin-right': '10px'},
+                        style={'width': '100%', 'justify-content': 'center', 'align-items': 'center', 'display': 'flex', 'height': '5vh'}
+                    ),
+                    dcc.Loading(
+                        html.Div([
+                            dcc.Graph(id='viewer-graph', style={'width': 'calc(100% - 50px)', 'height': 'inherit', 'float': 'left'}),
+                            html.Div([
+                                dcc.Slider(id='vertical-slider', vertical=True, min=0, max=100, step=1, value=50, marks={0: '0', 100: '100'})
+                            ], style={'width': '50px', 'height': '80vh', 'float': 'right', 'display': 'flex', "justify-content": "center", "align-items": "center"})
+                        ], style={'width': '100%', 'height': '80vh', 'display': 'flex', "justify-content": "center", "align-items": "center"}),
+                        id="loading",
+                        type="default",
+                        style={'width': '100%', 'height': '80vh', 'display': 'flex', "justify-content": "center", "align-items": "center"}
+                    ),
+                    dcc.RadioItems(
+                        id='component-selection',
+                        options=[
+                            {'label': 'Beam', 'value': 'Beam'},
+                            {'label': 'Scatter', 'value': 'Scatter'},
+                            {'label': 'All', 'value': 'All'}
+                        ],
+                        value='All',
+                        labelStyle={'display': 'inline-block', 'margin-right': '10px'},
+                        style={'width': '100%', 'justify-content': 'center', 'align-items': 'center', 'display': 'flex'}
+                    ),
+                    dcc.Checklist(id='enable-vertical-slicing', options=[{'label': 'Enable vertical slicing', 'value': 'vertical-slicing'}], value=['vertical-slicing'], style={'width': '100%', 'justify-content': 'center', 'align-items': 'center', 'display': 'flex'})
+                ])
+            elif tab == 'statistics':
+                return html.Div([
+                        html.H3("Statistics"),
+                        dcc.Loading(
+                            html.Div([
+                                dcc.Graph(id='statistics-graph', style={'width': '100%', 'height': 'inherit', 'float': 'left'}),
+                            ], style={'width': '100%', 'height': '80vh', 'display': 'flex', "justify-content": "center", "align-items": "center"}),
+                            id="loading",
+                            type="default",
+                            style={'width': '100%', 'height': '80vh', 'display': 'flex', "justify-content": "center", "align-items": "center"}
+                        ),
+                        dcc.Interval(id='progress-interval', interval=500, n_intervals=0, disabled=True),
+                        html.Progress(id='stats-progress-bar', value=0, max=100, style={'width': '100%'}),
+                    ])
+
+        @self.app.callback(
+            [
+                Output('statistics-graph', 'figure'),
+                Output('stats-progress-bar', 'value'),
+                Output('progress-interval', 'disabled')
+            ],
+            [
+                Input('statistics-list', 'value'),
+                Input('progress-interval', 'n_intervals')
+            ]
+        )
+        def update_statistics_graph(selected_statistics, n_intervals):
+            fig = go.Figure()
+            start_calc = selected_statistics and MainWindow.file_parsing_progress == 100 and MainWindow.last_statistics_calculated != selected_statistics
+            if start_calc:
+                MainWindow.last_statistics_calculated = selected_statistics
+                MainWindow.file_parsing_progress = 0
+                if 'energy' in selected_statistics:
+                    energies = []
+                    for i, file in enumerate(self.file_list):
+                        metadata: RadiationFieldMetadataHeaderV1 = FieldStore.peek_metadata(file).get_header()
+                        energies.append(metadata.simulation.tube.max_energy_eV / 1000.0)  # convert to keV
+                        MainWindow.file_parsing_progress = (i + 1) / len(self.file_list) * 100
+                    fig.add_trace(go.Histogram(x=energies, name='Energy in keV', nbinsx=int(max(energies))))
+                elif 'angles' in selected_statistics:
+                    alpha_angles = []
+                    beta_angles = []
+                    for i, file in enumerate(self.file_list):
+                        metadata: RadiationFieldMetadataHeaderV1 = FieldStore.peek_metadata(file).get_header()
+                        direction = metadata.simulation.tube.radiation_direction
+                        alpha = np.degrees(np.arccos(direction.x))  # Angle with x-axis
+                        beta = np.degrees(np.arccos(direction.y))   # Angle with y-axis
+                        alpha_angles.append(alpha)
+                        beta_angles.append(beta)
+                        MainWindow.file_parsing_progress = (i + 1) / len(self.file_list) * 100
+                    fig.add_trace(go.Histogram(x=alpha_angles, name='Alpha angle in °'))
+                    fig.add_trace(go.Histogram(x=beta_angles, name='Beta angle in °'))
+                elif 'source_distance' in selected_statistics:
+                    source_distances = []
+                    for i, file in enumerate(self.file_list):
+                        metadata: RadiationFieldMetadataHeaderV1 = FieldStore.peek_metadata(file).get_header()
+                        length = np.linalg.norm(np.array([metadata.simulation.tube.radiation_direction.x, metadata.simulation.tube.radiation_direction.y, metadata.simulation.tube.radiation_direction.z]))
+                        source_distances.append(length)
+                        MainWindow.file_parsing_progress = (i + 1) / len(self.file_list) * 100
+                    fig.add_trace(go.Histogram(x=source_distances, name='Source Distance'))
+
+            fig.update_layout(
+                template='plotly_dark',
+                paper_bgcolor='#2e2e2e',
+                plot_bgcolor='#2e2e2e',
+                font=dict(color='#ffffff')
+            )
+            return fig, str(MainWindow.file_parsing_progress), start_calc
 
         @self.app.callback(
             [
@@ -157,6 +263,9 @@ class MainWindow(FileSystemEventHandler):
 
             if not data or n_clicks:
                 self.file_list = [os.path.normpath(os.path.join(self.dataset_path, f)) for f in os.listdir(self.dataset_path) if f.endswith(".rf3")]
+                if len(self.file_list) == 0 and os.path.exists(os.path.join(self.dataset_path, "fields")):
+                    self.dataset_path = os.path.join(self.dataset_path, "fields")
+                    self.file_list = [os.path.normpath(os.path.join(self.dataset_path, f)) for f in os.listdir(self.dataset_path) if f.endswith(".rf3")]
 
                 if self.dir_watcher is not None:
                     self.dir_watcher.stop()
@@ -291,14 +400,12 @@ class MainWindow(FileSystemEventHandler):
         Thread(target=self.update_viewer_plot).start()
 
     def update_viewer_plot(self):
-        #self.enqueue_gui_call(lambda: self.window["-BUSY-GIF-"].update(visible=True))
-        #self.enqueue_gui_call(lambda: self.window["-VIEWER-PLOT-"].update(visible=False))
+        self.enqueue_gui_call(lambda: self.window["loading"].update(visible=True))
         plotter = RadSimPlotter(show_direction=self.show_radiation_source, infos=self.plotter.plot_infos, norm=self.norm)
         field = plotter.load_file(self.file_list[self.file_idx], self.plotter.plot_infos)
-        #self.enqueue_gui_call(lambda: self.window["-VIEWER-PLOT-"].update(visible=True))
         self.enqueue_gui_call(lambda: self.viewer_plot.draw_figure(plotter.plot_field(field)))
         self.enqueue_gui_call(lambda: self.caption_lbl.update(f"Plot: {os.path.basename(self.file_list[self.file_idx])}"))
-        #self.enqueue_gui_call(lambda: self.window["-BUSY-GIF-"].update(visible=False))
+        self.enqueue_gui_call(lambda: self.window["loading"].update(visible=False))
 
     def on_press_next(self):
         self.file_idx = (self.file_idx + 1) % len(self.file_list)
